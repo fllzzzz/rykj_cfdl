fd0be4fe5590749ab10d3ac2fcc93948b12bfc77
zhanghuajian <zhhj0104@qq.com>
2023-10-12 10:28:19 +0800

    no message

--- parking-services/src/main/java/com/cf/parking/services/service/ParkingSpaceChangeRecordService.java
+++ parking-services/src/main/java/com/cf/parking/services/service/ParkingSpaceChangeRecordService.java
@@ -1,9 +1,11 @@
 package com.cf.parking.services.service;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -74,31 +76,70 @@ public class ParkingSpaceChangeRecordService extends ServiceImpl<ParkingSpaceCha
 		record.setUpdateTm(new Date());
 		record.setState(ChangeRecordStateEnum.AGREE.getState());
 		spaceChangeRecordPOMapper.updateById(record);
-		//更新双方的车库、有效期信息
+		
 		UserInfoPO user = userProfileService.getUserInfoByUserId(record.getUserId());
 		AssertUtil.checkNull(user, "用户不存在");
 		
 		UserSpacePO space = new UserSpacePO();
-		//更新申请人的车库信息
-		space.setScheduleDate("")
+		//更新双方的车库、有效期信息
+		
+		//查询申请人是否有要交换的车位
+		List<UserSpacePO> applySpaceList = userSpaceService.querySpaceByJobNumAndParkLot(user.getJobNumber(), record.getAcceptParkingCode());
+		if(CollectionUtils.isEmpty(applySpaceList)) {
+			//不存在要交换的车库，直接更新原有的车库为交换的车库
+			space.setScheduleDate("")
 			.setState(UserSpaceStateEnum.UNSYNC.getState())
 			.setParkingLot(record.getAcceptParkingCode())
 			.setStartDate(record.getValidStartDate())
 			.setEndDate(record.getValidEndDate());
-		userSpaceService.update(space, new LambdaUpdateWrapper<UserSpacePO>()
+			userSpaceService.update(space, new LambdaUpdateWrapper<UserSpacePO>()
 									.eq(UserSpacePO::getJobNumber, user.getJobNumber())
 									.eq(UserSpacePO::getParkingLot, record.getParkingCode())
 									.eq(UserSpacePO::getType, UserSpaceTypeEnum.LOTTERY.getState())
 									.eq(UserSpacePO::getEndDate, DateUtil.format(record.getValidEndDate(), ParkingConstants.SHORT_DATE_FORMAT))
-		);
+					);
+			
+		} else {
+			//已经有要交换过来的车库，则更新车库有效期
+			applySpaceList.forEach(item -> {
+				item.setScheduleDate("")
+				.setState(UserSpaceStateEnum.UNSYNC.getState())
+				//开始日期用交换的车位日期和已有车位的开始日期的较小者
+				.setStartDate(DateUtil.beginOfDay(record.getValidStartDate()).compareTo(DateUtil.beginOfDay(item.getStartDate())) >= 0 ? item.getStartDate() : record.getValidStartDate())
+				//结束日期用两者中的较大者
+				.setEndDate(DateUtil.beginOfDay(record.getValidEndDate()).compareTo(DateUtil.beginOfDay(item.getEndDate())) >= 0 ? record.getValidEndDate() : item.getEndDate());
+			});
+			log.info("申请人：{}已存在交换车库：{},交换后原车库变更为：{}",user.getJobNumber(),record.getAcceptParkingCode(),JSON.toJSONString(applySpaceList));
+			userSpaceService.updateBatchById(applySpaceList);
+		}
+		
 		//更新交换人的车库信息
-		space.setParkingLot(record.getParkingCode());
-		userSpaceService.update(space, new LambdaUpdateWrapper<UserSpacePO>()
-				.eq(UserSpacePO::getJobNumber, param.getAcceptJobNumber())
-				.eq(UserSpacePO::getParkingLot, param.getAcceptParkingCode())
-				.eq(UserSpacePO::getType, UserSpaceTypeEnum.LOTTERY.getState())
-				.eq(UserSpacePO::getEndDate, DateUtil.format(record.getValidEndDate(), ParkingConstants.SHORT_DATE_FORMAT))
-		);
+		//查询交换人是否有要交换的车位
+		List<UserSpacePO> acceptSpaceList = userSpaceService.querySpaceByJobNumAndParkLot( param.getAcceptJobNumber(), record.getParkingCode());
+		if(CollectionUtils.isEmpty(acceptSpaceList)) {
+			//不存在要交换的车库，直接更新原有的车库为交换的车库
+			space.setParkingLot(record.getParkingCode());
+			userSpaceService.update(space, new LambdaUpdateWrapper<UserSpacePO>()
+					.eq(UserSpacePO::getJobNumber, param.getAcceptJobNumber())
+					.eq(UserSpacePO::getParkingLot, param.getAcceptParkingCode())
+					.eq(UserSpacePO::getType, UserSpaceTypeEnum.LOTTERY.getState())
+					.eq(UserSpacePO::getEndDate, DateUtil.format(record.getValidEndDate(), ParkingConstants.SHORT_DATE_FORMAT))
+			);
+			
+		} else {
+			//已经有要交换过来的车库，则更新车库有效期
+			acceptSpaceList.forEach(item -> {
+				item.setScheduleDate("")
+				.setState(UserSpaceStateEnum.UNSYNC.getState())
+				//开始日期用交换的车位日期和已有车位的开始日期的较小者
+				.setStartDate(DateUtil.beginOfDay(record.getValidStartDate()).compareTo(DateUtil.beginOfDay(item.getStartDate())) >= 0 ? item.getStartDate() : record.getValidStartDate())
+				//结束日期用两者中的较大者
+				.setEndDate(DateUtil.beginOfDay(record.getValidEndDate()).compareTo(DateUtil.beginOfDay(item.getEndDate())) >= 0 ? record.getValidEndDate() : item.getEndDate());
+			});
+			log.info("交换人：{}已存在交换车库：{},交换后原车库变更为：{}",param.getAcceptJobNumber(),record.getParkingCode(),JSON.toJSONString(acceptSpaceList));
+			userSpaceService.updateBatchById(acceptSpaceList);
+		}		
+		
 	}
 
 }
